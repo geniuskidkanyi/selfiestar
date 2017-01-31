4b7f55f4d3376c75752e2886bff818c14a03c6cd
geniuskid kanyi <kanyi@gcubed.gm>
2017-01-31 17:56:21 +0000

    fixed ex6

--- app/assets/javascripts/selfies.js
+++ app/assets/javascripts/selfies.js
@@ -1,34 +1,39 @@
-const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-$(document).on('ready turbolinks:load', function() {
+'use strict';
+
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+$(document).on('ready turbolinks:load', function () {
 
     $.fn.extend({
-        animateCss(animationName, hide = false) {
-            $(this).addClass(`animated ${animationName}`).one(animationEnd, e => {
-                $(this).removeClass('animated ' + animationName)
-                if (hide) $(this).hide()
+        animateCss: function animateCss(animationName) {
+            var _this = this;
+
+            var hide = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+            $(this).addClass('animated ' + animationName).one(animationEnd, function (e) {
+                $(_this).removeClass('animated ' + animationName);
+                if (hide) $(_this).hide();
             });
         }
     });
 
-
-    $('.show_lightbox').on('click', function(e) {
+    $('.show_lightbox').on('click', function (e) {
         e.preventDefault();
-        let $selfie_id = $(this).data('selfie-id');
-        let src = e.target.src
+        var $selfie_id = $(this).data('selfie-id');
+        var src = e.target.src;
 
-        $('.lightbox_container_image img').attr('src', src)
-         $('.lightbox').show().animateCss('fadeIn')
+        $('.lightbox_container_image img').attr('src', src);
+        $('.lightbox').show().animateCss('fadeIn');
 
         $.ajax({
             type: "get",
             url: "/selfies/" + $selfie_id
         });
-    })
+    });
 
-    $('.lightbox').not($('.lightbox_container')).on('click', function(e) {
-        if (e.target !== this) return
-        e.preventDefault()
-        e.stopPropagation()
-        $(this).animateCss('fadeOut', true)
-    })
-})
+    $('.lightbox').not($('.lightbox_container')).on('click', function (e) {
+        if (e.target !== this) return;
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).animateCss('fadeOut', true);
+    });
+});
