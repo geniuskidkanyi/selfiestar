3a0575409b12dd41fc3a85272c4e16a55f440540
geniuskid kanyi <kanyi@gcubed.gm>
2017-02-01 13:42:49 +0000

    fix the trending

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,7 +25,7 @@ def trending
   date = DateTime.now.utc
   likes = Like.where('created_at >= ? and created_at <= ?', date.beginning_of_week, date.utc.end_of_week).select(:selfy_id)
   top = current_user.selfies.where(id: likes)
-  top.order("COALESCE(likes_count, 0) DESC").limit(10)
+  top.order("COALESCE(likes_count, 0) DESC").limit(5)
 end
 
 
