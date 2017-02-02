c58c19ae5a4105061ac1e0a0cdca9a88f73379b9
geniuskid kanyi <kanyi@gcubed.gm>
2017-02-02 19:42:59 +0000

    added about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,9 @@ class PagesController < ApplicationController
   def upload
     @selfie = Selfy.new
   end
-
+  def about
+    #code
+  end
   def profile
     @user = User.find(params[:id])
     @week_trendings = trending
@@ -24,13 +26,13 @@ class PagesController < ApplicationController
     date = DateTime.now.utc
     likes = Like.where('created_at >= ? and created_at <= ?', date.beginning_of_week, date.utc.end_of_week).select(:selfy_id)
     top = current_user.selfies.where(id: likes)
-    top.order("COALESCE(likes_count, 0) DESC").limit(5)
+    top.order("COALESCE(likes_count, 0) DESC").limit(6)
   end
 def trending_wide
   date = DateTime.now.utc
   likes = Like.where('created_at >= ? and created_at <= ?', date.beginning_of_week, date.utc.end_of_week).select(:selfy_id)
   top = Selfy.where(id: likes)
-  top.order("COALESCE(likes_count, 0) DESC").limit(5)
+  top.order("COALESCE(likes_count, 0) DESC").limit(6)
 end
 
 
