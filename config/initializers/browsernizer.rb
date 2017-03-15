ffc9a7adf27191fb8e6977ac4168d3ae0d8c8770
geniuskid kanyi <kanyi@gcubed.gm>
2017-03-15 11:55:10 +0000

    added browser happy

--- config/initializers/browsernizer.rb
+++ config/initializers/browsernizer.rb
@@ -1,16 +1,19 @@
 Rails.application.config.middleware.use Browsernizer::Router do |config|
   config.supported "Edge", "13"
   config.supported "Firefox", "51"
-  config.supported "Opera", "43"
+  config.supported "Opera", false
   config.supported "Chrome", "4"
-  config.supported "Android", "4.0"
-  config.supported "BlackBerry", "51"
+  config.supported "Android", "4.4"
+  config.supported "BlackBerry", false
+    config.supported "nokia S40 Ovi Browser", false
   config.supported "Safari", "8"
-  config.supported "Internet Explorer", "11"
-  config.supported "Samsung Internet", "3"
-  config.supported "Android Browser", "4.0"
+  config.supported "Internet Explorer", "13"
+  config.supported "Samsung Internet", "4"
+  config.supported "Android Browser", "4.4"
   config.supported "iOS Safari", "8.0"
   config.supported "UCBrowser",  false
+  config.supported "QQ Browser",  false
+  config.supported "Generic Browser",  false
   config.location  "/browser.html"
   config.exclude   %r{^/assets}
 end
