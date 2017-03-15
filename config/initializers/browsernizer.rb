e0a3f832340ab576d663e0bd8d1a869b0ae546d7
geniuskid kanyi <kanyi@gcubed.gm>
2017-03-15 10:50:43 +0000

    updated

--- config/initializers/browsernizer.rb
+++ config/initializers/browsernizer.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use Browsernizer::Router do |config|
   config.supported "Edge", "13"
   config.supported "Firefox", "51"
   config.supported "Opera", "43"
-  config.supported "Chrome", "49"
+  config.supported "Chrome", "4"
   config.supported "Android", "4.0"
   config.supported "BlackBerry", "51"
   config.supported "Safari", "10"
