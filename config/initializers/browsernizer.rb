6a50a3fdcccd5d71c6f49c7b12af5928a9618dc0
geniuskid kanyi <kanyi@gcubed.gm>
2017-03-15 10:43:13 +0000

    updated the browser support

--- config/initializers/browsernizer.rb
+++ config/initializers/browsernizer.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use Browsernizer::Router do |config|
   config.supported "Firefox", "51"
   config.supported "Opera", "43"
   config.supported "Chrome", "49"
-  config.supported "Android", "4.4"
+  config.supported "Android", "4.0"
   config.supported "BlackBerry", "51"
   config.supported "Safari", "10"
   config.supported "Internet Explorer", "11"
