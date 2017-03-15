c8da3171a37cf8620e391e62bc2bc812c5389335
geniuskid kanyi <kanyi@gcubed.gm>
2017-03-15 10:56:48 +0000

    change for ios

--- config/initializers/browsernizer.rb
+++ config/initializers/browsernizer.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use Browsernizer::Router do |config|
   config.supported "Chrome", "4"
   config.supported "Android", "4.0"
   config.supported "BlackBerry", "51"
-  config.supported "Safari", "10"
+  config.supported "Safari", "8"
   config.supported "Internet Explorer", "11"
   config.location  "/browser.html"
   config.exclude   %r{^/assets}
